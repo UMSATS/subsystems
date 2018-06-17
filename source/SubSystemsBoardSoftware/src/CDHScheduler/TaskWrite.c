23006e8e618b5873a4aea03b7f9ade577802dc9e
rahmant3 <rahmant3@myumanitoba.ca>
2018-06-17 13:57:33 -0600

    Check that the default value wasn't returned by SerialRead.

--- source/SubSystemsBoardSoftware/src/CDHScheduler/TaskWrite.c
+++ source/SubSystemsBoardSoftware/src/CDHScheduler/TaskWrite.c
@@ -9,7 +9,7 @@
 // 2018-06-11 by Tamkin Rahman
 // - Move input mocking to "changeMockPower" task (renamed to MockInput).
 // 2018-06-15 by Tamkin Rahman
-// - Add delay at the end of "MockInput"'s while loop.
+// - Add delay at the end of "MockInput"'s while loop. Check that the default value wasn't returned.
 
 // -----------------------------------------------------------------------------------------------
 // ----------------------- INCLUDES --------------------------------------------------------------
@@ -79,67 +79,71 @@ void MockInput(void *params) {
 	vTaskDelayUntil(&lastWakeTime, frequency);	 
 	received = SerialRead(powerReading);
   
-	if (received == 'A') 
+	if (received != powerReading)
 	{
-		valueRead = 10;
-	} 
-	else if (received == 'B') 
-	{
-		valueRead = 90;
-	}
-	else if (received == 'C') 
-	{
-		AddToTXQueue(&message);
-		SerialPrint("Added to TX queue\r\n");
-	}
-	else if (received == 'D') 
-	{
-		message.id = POWER_ID;
-		message.length = 8;
-		message.data.bytes[0] = 0x50;
-		message.data.bytes[1] = 0x00;
-		message.data.bytes[2] = 0x00;
-		message.data.bytes[3] = 0x00;
+	
+		if (received == 'A') 
+		{
+			valueRead = 10;
+		} 
+		else if (received == 'B') 
+		{
+			valueRead = 90;
+		}
+		else if (received == 'C') 
+		{
+			AddToTXQueue(&message);
+			SerialPrint("Added to TX queue\r\n");
+		}
+		else if (received == 'D') 
+		{
+			message.id = POWER_ID;
+			message.length = 8;
+			message.data.bytes[0] = 0x50;
+			message.data.bytes[1] = 0x00;
+			message.data.bytes[2] = 0x00;
+			message.data.bytes[3] = 0x00;
     
-		AddToRXQueue(&message);
-		SerialPrint("Added to RX queue\r\n");
-	}
-	else if (received == 'E')
-	{
-		message.id = PAYLOAD_ID;
-		message.length = 8;
-		message.data.bytes[0] = 1;  // Well number
-		// Reading and reserved bytes not set.
+			AddToRXQueue(&message);
+			SerialPrint("Added to RX queue\r\n");
+		}
+		else if (received == 'E')
+		{
+			message.id = PAYLOAD_ID;
+			message.length = 8;
+			message.data.bytes[0] = 1;  // Well number
+			// Reading and reserved bytes not set.
     
-		AddToRXQueue(&message);
-		SerialPrint("Added to RX queue\r\n");
-	}
-	else if (received == 'F')
-	{
-		message.id = GROUND_STATION;
-		message.length = 8;
-		message.data.GroundStationData.command = TURN_ON_WELL; // Command.
-		message.data.GroundStationData.dataBytes.payLoadCommand.wellNumber = 1; // Well number.
+			AddToRXQueue(&message);
+			SerialPrint("Added to RX queue\r\n");
+		}
+		else if (received == 'F')
+		{
+			message.id = GROUND_STATION;
+			message.length = 8;
+			message.data.GroundStationData.command = TURN_ON_WELL; // Command.
+			message.data.GroundStationData.dataBytes.payLoadCommand.wellNumber = 1; // Well number.
 
-		// Wait 1 second until using this command.
-		message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[0] = 1;
-		message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[1] = 0;
-		message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[2] = 0;
-		message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[3] = 0;
+			// Wait 1 second until using this command.
+			message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[0] = 1;
+			message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[1] = 0;
+			message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[2] = 0;
+			message.data.GroundStationData.dataBytes.payLoadCommand.secondsUntilCommandLSB[3] = 0;
     
-		// Reading and reserved bytes not set.
+			// Reading and reserved bytes not set.
     
-		AddToRXQueue(&message);
-		SerialPrint("Added to RX queue\r\n");
-	}    
+			AddToRXQueue(&message);
+			SerialPrint("Added to RX queue\r\n");
+		}    
 
     
-	if ( valueRead != powerReading ) {
-		if ( (valueRead > POWER_MIN) && (valueRead <= POWER_MAX) ){
-			powerReading = valueRead;
-			SerialPrint("Changed the power to ");
-			SerialPrintInt(powerReading);
-			SerialPrint("\r\n");
+		if ( valueRead != powerReading ) {
+			if ( (valueRead > POWER_MIN) && (valueRead <= POWER_MAX) ){
+				powerReading = valueRead;
+				SerialPrint("Changed the power to ");
+				SerialPrintInt(powerReading);
+				SerialPrint("\r\n");
+			}
 		}
 	}
 	
